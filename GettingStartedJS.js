30698eac5a2afe9b3d8d230bf4de8d51dceaad3a
Brendan Tea <teke453jr@gmail.com>
2023-08-27 17:52:39 -0400

    final commit with fixes and comments added

--- GettingStartedJS.js
+++ GettingStartedJS.js
@@ -1,6 +1,9 @@
-/*Brendan Tea
-Getting Started Javascript file
-08/27/2023
+/*
+    Title: GettingStartedJS.js
+    Author: Brendan Tea
+    Purpose: To get familiar with HTML, CSS, and Javascript to make a simple website
+    Originally created on: 8/27/23
+    Last modified on: 8/27/23
 */
 
 function time() {
