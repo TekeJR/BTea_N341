342fefd5359a8289efdccb6ea6227d2b47659c42
Brendan Tea <teke453jr@gmail.com>
2023-09-10 00:07:43 -0400

    arithmetic and string manipulation working

--- BTeaLab1.js
+++ BTeaLab1.js
@@ -15,5 +15,27 @@ function output(ID, newMessage) {
 }
 
 output("message", "Welcome Brendan!");
-output("footer", "Thank you for ordering from us!")
-output("GarlicTonkDesc", "Our most popular ramen! Customers are satisfied everytime!")
+output("footer", "Thank you for ordering from us!");
+output("GarlicTonkDesc", "Our most popular ramen! Customers are satisfied everytime!");
+
+const ramen = ["Garlic Tonkatsu", " Tonkatsu", " Shoyu", " Miso", " Shio"];
+const eggs = [1, 2, 3, 4, 5];
+
+output("ramenTypes", ramen);
+output("eggNum", eggs);
+
+ramen[0] = ramen[0].slice(3, 11);
+ramen[1] = ramen[1].replace("Tonkatsu", "Veggie");
+ramen[2] = ramen[2].toLowerCase();
+ramen[3] = ramen[3].toUpperCase();
+ramen[4] = ramen[4].concat(" and ", ramen[2]);
+
+output("ramenTypesALT", ramen);
+
+eggs[0] = eggs[0] + 18;
+eggs[1] = eggs[1] - 3;
+eggs[2] = eggs[2] / 3;
+eggs[3] = eggs[3] * 4;
+eggs[4] = eggs[4] % 2;
+
+output("eggNumALT", eggs);
