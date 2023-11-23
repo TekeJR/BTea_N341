6202e84bd169d75bd5d71eabf1d8e8569997f01b
Brendan Tea <teke453jr@gmail.com>
2023-11-22 22:52:00 -0500

    base structure, initial commit

---
+++ Lab5/BTeaLab5.js
@@ -0,0 +1,8 @@
+/*
+    Title: BTeaMidterm.js
+    Author: Brendan Tea
+    Purpose: Culmination of Javascript skills thus far
+    Originally created on: 11/22/23
+    Last modified on: 11/22/23
+*/
+
